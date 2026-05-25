2175eef725917041080bdd0f3e0a1fd215f07b64
Azimut-27 <martin.jancar1@gmail.com>
2026-05-25 21:55:08 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles.css";
-import WorkoutGenerator from "./WorkoutGenerator_v45.144.jsx";
+import WorkoutGenerator from "./ForgeAI_Current_WorkoutGenerator_v45.144.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
